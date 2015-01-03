393ee1d9ba536bfbdc4cb765a558604823af6776
Wachter Johannes <johannes.wachter@massiveart.at>
2015-01-03 14:12:05 +0100

    init websocket-bundle

---
+++ src/Vlbg/Bundle/WebsocketBundle/Entity/Entry.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Vlbg\Bundle\WebsocketBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Entry
+ */
+class Entry
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     */
+    private $created;
+
+    /**
+     * @var string
+     */
+    private $creator;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Entry
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set creator
+     *
+     * @param string $creator
+     * @return Entry
+     */
+    public function setCreator($creator)
+    {
+        $this->creator = $creator;
+
+        return $this;
+    }
+
+    /**
+     * Get creator
+     *
+     * @return string 
+     */
+    public function getCreator()
+    {
+        return $this->creator;
+    }
+}
---
+++ src/Vlbg/Bundle/WebsocketBundle/VlbgWebsocketBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Vlbg\Bundle\WebsocketBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VlbgWebsocketBundle extends Bundle
+{
+}
